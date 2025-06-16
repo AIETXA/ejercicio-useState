22aa0e3b5d7913a00e2baca155b18e3f4b488865
Ailén <ailenechaguibel@gmail.com>
2025-06-16 09:06:38 +0200

    ejercicio sin bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import './App.css'
+import { useState } from 'react'
+
+
+
 
 function App() {
- //Aquí tu código
-}
+    const [ name, setName ] = useState('Sofía');
+    const [ newName, setNewName ] = useState('');
+
+return (
+    <div> 
+        <h2>Teacher name: {name}</h2>
+      
+      <ul>
+        <li><button onClick={()=>setName('Data')}>Data</button></li>
+        <li><button onClick={()=>setName('Reyes')}>Reyes</button></li>
+        <li><button onClick={()=>setName('Yolanda')}>Yolanda</button></li>
+      </ul>
+    </div>
+    )
+};
+
 
 export default App
